b5a992a899dd93f3561080bbfdcdf01baf17d3d0
Megha vk <meghamurali2324@gmail.com>
2025-09-19 18:25:45 +0530

    all files

---
+++ delay_t0_normal/delay_t0_normal/main.c
@@ -0,0 +1,28 @@
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+
+void delay_1s(void){
+	unsigned int i;
+	unsigned int count=976;
+	for(i=0; i<count; i++){
+		TCNT0=0;
+		TCCR0A=0;
+		TCCR0B|=(1<<CS00);
+		TCCR0B|=(1<<CS01);
+		TIFR0|=(1<<TOV0);
+		
+		while(!(TIFR0 & (1<<TOV0)));
+	}
+}
+int main(void)
+{
+	DDRB|=(1<<DDB5);
+	while (1)
+	{
+		PORTB^=(1<<PORTB5);
+		delay_1s();
+
+	}
+}
+
